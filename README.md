14946e2aed4acbfa62af5b4c6cf653c8d8a76e83
Illia Polosukhin <ilblackdragon@gmail.com>
2020-12-10 00:46:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proxy server for Ethereum JSON RPC to NEAR EVM
+
+Provides a JSON RPC interface compatible with [Ethereum's API](https://eth.wiki/json-rpc/API) to NEAR's EVM environment.
+
+## Usage
+
+To install and start the local server, run:
+```
+npm install
+npm run dev
+```
+
+In your wallet, truffle, etc - change node url to the url of this server.
+
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const { NearProvider } = require('near-web3-provider');
+const app = express()
+const port = 3000
+
+const provider = new NearProvider({
+  nodeUrl: 'http://rpc.betanet.near.org',
+  networkId: 'default',
+  evmAccountId: 'evm',
+  masterAccountId: 'illia.betanet',
+});
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+function response(id, result, error) {
+  let resp = {
+    jsonrpc: "2.0",
+    id,
+  };
+  if (result) {
+    resp.result = result;
+  } else {
+    resp.error = error;
+  }
+  return resp;
+}
+
+app.post('/', async (req, res) => {
+  res.header('Content-Type', 'application/json');
+  const data = req.body;
+  // TODO: validate data input is correct JSON RPC.
+  console.log(data, req.params);
+  try {
+    const result = await provider.routeRPC(data.method, data.params);
+    console.log(result);
+    res.send(response(data.id, result, null));
+  } catch (error) {
+    console.log(error);
+    // TODO: return errors that match errors from Ethereum nodes.
+    res.send(response(data.id, null, error));
+  }
+})
+
+app.listen(port, () => {
+  console.log(`NEAR EVM JSON RPC Proxy listening at http://localhost:${port}`)
+});
