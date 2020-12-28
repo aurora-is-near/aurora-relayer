e5a62082801b3430a4542d2893b6688ab9ad3c72
Illia Polosukhin <illia@nearprotocol.com>
2020-12-28 17:15:30 +0800

    Convert to TypeScript. (#1)

---
+++ index.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import NearProvider from 'near-web3-provider';
+
+const app = express()
+const port = 3030
+
+const provider: NearProvider = new NearProvider.NearProvider({
+  nodeUrl: 'http://rpc.betanet.near.org',
+  networkId: 'betanet',
+  evmAccountId: 'evm',
+  masterAccountId: 'testevm1.betanet',
+});
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+function response(id, result, error) {
+  let resp = {
+    jsonrpc: "2.0",
+    id,
+  };
+  if (result) {
+    Object.assign(resp, { result });
+  } else {
+    Object.assign(resp, { error });
+  }
+  return resp;
+}
+
+app.post('/', async (req, res) => {
+  res.header('Content-Type', 'application/json');
+  const data = req.body;
+  // TODO: validate data input is correct JSON RPC.
+  console.log(data, req.params);
+  try {
+    const result = await provider.routeRPC(data.method, data.params);
+    console.log(result);
+    res.send(response(data.id, result, null));
+  } catch (error) {
+    console.log(error);
+    // TODO: return errors that match errors from Ethereum nodes.
+    res.send(response(data.id, null, error));
+  }
+})
+
+app.listen(port, () => {
+  console.log(`NEAR EVM JSON RPC Proxy listening at http://localhost:${port}`)
+});
