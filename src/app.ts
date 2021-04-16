25df664e7d0115e3b93d7da7456b26300fdcb78e
Arto Bendiken <arto@near.org>
2021-04-16 14:48:42 +0300

    Improve Geth compatibility of RPC responses.

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,48 @@ interface NearProvider {
     explorerUrl: string;
 }
 
+abstract class CodedError extends Error {
+    constructor(public readonly code: number, message: string) {
+        super(message);
+        Object.setPrototypeOf(this, CodedError.prototype);
+    }
+}
+
+class UnsupportedMethod extends CodedError {
+    constructor(method: string) {
+        super(-32601, `Unsupported method: ${method}`);
+        Object.setPrototypeOf(this, UnsupportedMethod.prototype);
+    }
+}
+
+class UnimplementedMethod extends CodedError {
+    constructor(method: string) {
+        super(-32601, `Unimplemented method: ${method}`);
+        Object.setPrototypeOf(this, UnimplementedMethod.prototype);
+    }
+}
+
+class MissingArgument extends CodedError {
+    constructor(index: number, message?: string) {
+        super(-32602, message || `missing value for required argument ${index}`);
+        Object.setPrototypeOf(this, MissingArgument.prototype);
+    }
+}
+
+class TooManyArguments extends CodedError {
+    constructor(maxCount: number) {
+        super(-32602, `too many arguments, want at most ${maxCount}`);
+        Object.setPrototypeOf(this, TooManyArguments.prototype);
+    }
+}
+
+class InvalidArguments extends CodedError {
+    constructor() {
+        super(-32602, `Invalid method parameter(s).`);
+        Object.setPrototypeOf(this, InvalidArguments.prototype);
+    }
+}
+
 function response(id: string, result: any, error: any) {
     const resp = { jsonrpc: '2.0', id };
     if (error) {
@@ -33,6 +75,13 @@ function response(id: string, result: any, error: any) {
         Object.assign(resp, { result });
     }
     return resp;
+}
+
+function errorCode(error: Error) {
+    if (error instanceof CodedError) {
+        return error.code;
+    }
+    return -32000;
 }
 
 export function createApp(argv: any, provider: NearProvider) {
@@ -59,9 +108,8 @@ export function createApp(argv: any, provider: NearProvider) {
             if (argv.failHard) {
                 exit(0);
             }
-            // TODO: return errors that match errors from Ethereum nodes.
             res.send(response(data.id, null, {
-                code: -32000,
+                code: errorCode(error),
                 message: error.message,
             }));
         }
@@ -99,45 +147,59 @@ export function createApp(argv: any, provider: NearProvider) {
     return app;
 }
 
-function unsupported() {
-    throw new Error("unsupported");
+function unsupported(method: string) {
+    throw new UnsupportedMethod(method);
 }
 
-function unimplemented() {
-    throw new Error("unimplemented");
+function unimplemented(method: string) {
+    throw new UnimplementedMethod(method);
 }
 
 export async function routeRPC(provider: NearProvider, method: string, params: any[]): Promise<any> {
     const engine = await Engine.connect({});
+    //console.log(method, params); // DEBUG
     switch (method) {
         // web3_*
-        case 'web3_clientVersion': return 'Aurora-Relayer';
-        case 'web3_sha3': return `0x${Buffer.from(keccakFromHexString(params[0])).toString('hex')}`;
+        case 'web3_clientVersion': {
+            if (params.length > 0) throw new TooManyArguments(0);
+            return 'Aurora-Relayer/0.0.0'; // TODO
+        }
+        case 'web3_sha3': {
+            if (params.length < 1) throw new MissingArgument(0);
+            if (params.length > 1) throw new TooManyArguments(1);
+            return `0x${Buffer.from(keccakFromHexString(params[0])).toString('hex')}`;
+        }
 
         // net_*
-        case 'net_listening': return false;
+        case 'net_listening': return true;
         case 'net_peerCount': return '0x0';
-        case 'net_version': return '1';
+        case 'net_version': {
+            const chainID = (await engine.getChainID()).unwrap();
+            return `0x${chainID.toString(16)}`;
+        }
 
         // eth_*
         case 'eth_accounts': break; // TODO
         case 'eth_blockNumber': {
-            const chainID = (await engine.getBlockHeight()).unwrap();
-            return `0x${chainID.toString(16)}`;
+            const height = (await engine.getBlockHeight()).unwrap();
+            return `0x${height.toString(16)}`;
         }
         case 'eth_call': break; // TODO
         case 'eth_chainId': { // EIP-695
-            return (await engine.getChainID()).unwrap().toString();
+            const chainID = (await engine.getChainID()).unwrap();
+            return `0x${chainID.toString(16)}`;
         }
         case 'eth_coinbase': {
             return (await engine.getCoinbase()).unwrap();
         }
-        case 'eth_compileLLL': return unsupported();
-        case 'eth_compileSerpent': return unsupported();
-        case 'eth_compileSolidity': return unsupported();
+        case 'eth_compileLLL': return unsupported(method);
+        case 'eth_compileSerpent': return unsupported(method);
+        case 'eth_compileSolidity': return unsupported(method);
         case 'eth_estimateGas': break; // TODO
         case 'eth_gasPrice': return '0x0';
         case 'eth_getBalance': {
+            if (params.length < 1) throw new MissingArgument(0);
+            if (params.length > 2) throw new InvalidArguments();
             const balance = (await engine.getBalance(params[0])).unwrap();
             return `0x${balance.toString(16)}`;
         }
@@ -146,6 +208,8 @@ export async function routeRPC(provider: NearProvider, method: string, params: a
         case 'eth_getBlockTransactionCountByHash': break; // TODO
         case 'eth_getBlockTransactionCountByNumber': break; // TODO
         case 'eth_getCode': {
+            if (params.length < 1) throw new MissingArgument(0);
+            if (params.length > 2) throw new InvalidArguments();
             const code = (await engine.getCode(params[0])).unwrap();
             return `0x${Buffer.from(code).toString('hex')}`;
         }
@@ -155,6 +219,9 @@ export async function routeRPC(provider: NearProvider, method: string, params: a
         case 'eth_getLogs': break; // TODO
         case 'eth_getProof': break; // EIP-1186 TODO
         case 'eth_getStorageAt': {
+            if (params.length < 1) throw new MissingArgument(0);
+            if (params.length < 2) throw new MissingArgument(1);
+            if (params.length > 3) throw new InvalidArguments();
             const result = (await engine.getStorageAt(params[0], params[1])).unwrap();
             return formatU256(result);
         }
@@ -162,38 +229,40 @@ export async function routeRPC(provider: NearProvider, method: string, params: a
         case 'eth_getTransactionByBlockNumberAndIndex': break; // TODO
         case 'eth_getTransactionByHash': break; // TODO
         case 'eth_getTransactionCount': {
+            if (params.length < 1) throw new MissingArgument(0, "cannot request transaction count without specifying address");
+            if (params.length > 2) throw new InvalidArguments();
             const nonce = (await engine.getNonce(params[0])).unwrap();
-            return formatU256(nonce);
+            return `0x${nonce.toString(16)}`;
         }
         case 'eth_getTransactionReceipt': break; // TODO
         case 'eth_getUncleByBlockHashAndIndex': break; // TODO
         case 'eth_getUncleByBlockNumberAndIndex': break; // TODO
         case 'eth_getUncleCountByBlockHash': return '0x0';
         case 'eth_getUncleCountByBlockNumber': return '0x0';
-        case 'eth_getWork': return unsupported();
+        case 'eth_getWork': return unsupported(method);
         case 'eth_hashrate': return '0x0';
         case 'eth_mining': return false;
-        case 'eth_newBlockFilter': return unimplemented(); // TODO
-        case 'eth_newFilter': return unimplemented(); // TODO
-        case 'eth_newPendingTransactionFilter': return unimplemented(); // TODO
+        case 'eth_newBlockFilter': return unimplemented(method); // TODO
+        case 'eth_newFilter': return unimplemented(method); // TODO
+        case 'eth_newPendingTransactionFilter': return unimplemented(method); // TODO
         case 'eth_pendingTransactions': return [];
-        case 'eth_protocolVersion': return '1'; // FIXME
+        case 'eth_protocolVersion': return '0x41';
         case 'eth_sendRawTransaction': break; // TODO
         case 'eth_sendTransaction': break; // TODO
         case 'eth_sign': break; // TODO
         case 'eth_signTransaction': break; // TODO
         case 'eth_signTypedData': break; // EIP-712 TODO
-        case 'eth_submitHashrate': return unsupported();
-        case 'eth_submitWork': return unsupported();
+        case 'eth_submitHashrate': return unsupported(method);
+        case 'eth_submitWork': return unsupported(method);
         case 'eth_syncing': return false;
-        case 'eth_uninstallFilter': return unimplemented();
+        case 'eth_uninstallFilter': return unimplemented(method);
 
         // near_*
         case 'near_retrieveNear': break;
         case 'near_transferNear': break;
 
         // *
-        default: return unsupported();
+        default: return unsupported(method);
     }
     return await (provider as any).routeRPC(method, params);
 }
