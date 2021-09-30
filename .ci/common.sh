f543fbd1bcb7cc1030a754e2c0173befb1ddf3de
Dmitry Strokov <dmitry.strokov@aurora.dev>
2021-09-30 05:17:06 +0300

    Add RPC-tests CI

---
+++ .ci/common.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -ev
+
+
+# All CI-runners are docker containers, and creating new
+# Docker objects (images, containers, etc) from some runner
+# will make them appear on host-level Docker, since
+# socket-binding solution is used for Docker-in-Docker. 
+#
+# To avoid collision of such objects, $RUNNER_NAME suffix
+# is added to every object's name.
+
+UNIQUE_KEY=$RUNNER_NAME
+
+NETWORK_NAME=network-for-$UNIQUE_KEY
+DATABASE_IMAGE_NAME=relayer-database-img-$UNIQUE_KEY
+ENDPOINT_IMAGE_NAME=relayer-endpoint-img-$UNIQUE_KEY
+DATABASE_CONTAINER_NAME=relayer-database-$UNIQUE_KEY
+INDEXER_CONTAINER_NAME=relayer-indexer-$UNIQUE_KEY
+ENDPOINT_CONTAINER_NAME=relayer-endpoint-$UNIQUE_KEY
+NEARCORE_CONTAINER_NAME=nearcore-$UNIQUE_KEY
+
+
+# Version locks.
+# Specific versions of each dependency are used
+# in order to provide consistent CI.
+
+NEAR_CLI_HEAD=e7a2c8fcd428b6ae006dcf5340b1469d9ec815b0
+# Commit hash from: https://github.com/near/near-cli/commits/master
+
+AURORA_CLI_HEAD=7f7c2d29c5114db2b6530a6c0fa09b2068f217df
+# Commit hash from: https://github.com/aurora-is-near/aurora-cli/commits/master
+
+NEARCORE_TAG=master-8a2be9ce7768fc4b58558ad73dacfc794b5bef4f
+# Docker image tag from: https://hub.docker.com/r/nearprotocol/nearcore/tags
+
+CONTRACT_URL=https://github.com/aurora-is-near/aurora-engine/releases/download/1.6.4/mainnet-release.wasm
+# Download url from: https://github.com/aurora-is-near/aurora-engine/releases
---
+++ .ci/setup.sh
@@ -0,0 +1,143 @@
+#!/bin/bash -ev
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source SCRIPT_DIR/common.sh
+cd SCRIPT_DIR/..
+
+
+echo "Environment will be cleaned to avoid dirty start"
+./.ci/cleanup.sh
+
+
+echo "Establishing inner network..."
+docker network create -d bridge $NETWORK_NAME
+docker network connect $NETWORK_NAME $RUNNER_NAME
+
+
+echo "Building database image..."
+time docker build -t $DATABASE_IMAGE_NAME -f .docker/Dockerfile.database .
+
+
+echo "Building endpoint image..."
+time docker build -t $ENDPOINT_IMAGE_NAME -f .docker/Dockerfile.endpoint .
+
+
+mkdir .ci/workdir
+cd .ci/workdir
+
+echo "Installing near-cli..."
+git clone https://github.com/near/near-cli.git
+cd near-cli
+git checkout $NEAR_CLI_HEAD
+time npm ci
+cd ..
+
+echo "Installing aurora-cli..."
+git clone https://github.com/aurora-is-near/aurora-cli.git
+cd aurora-cli
+git checkout $AURORA_CLI_HEAD
+time npm ci
+cd ..
+
+mkdir nearData
+cd ..
+
+
+echo "Starting nearcore..."
+docker run -it --rm \
+    -v $(pwd)/.ci/workdir/nearData:/srv/near \
+    --name $NEARCORE_CONTAINER_NAME \
+    nearprotocol/nearcore:$NEARCORE_TAG \
+    neard --home=/srv/near init
+docker run -d --rm \
+    -v $(pwd)/.ci/workdir/nearData:/srv/near \
+    --network $NETWORK_NAME \
+    --name $NEARCORE_CONTAINER_NAME \
+    nearprotocol/nearcore:$NEARCORE_TAG \
+    neard --home=/srv/near run
+
+echo "Sleeping for 5 seconds..."
+sleep 5
+
+
+export NEAR_ENV=local
+
+echo "Creating NEAR account..."
+.ci/workdir/near-cli/bin/near create-account aurora.test.near \
+    --master-account=test.near \
+    --initial-balance 1000000 \
+    --key-path .ci/workdir/nearData/validator_key.json \
+    --node-url https://${NEARCORE_CONTAINER_NAME}:3030
+
+echo "Downloading contract..."
+curl -L $CONTRACT_URL -o .ci/workdir/contract.wasm
+
+echo "Installing contract..."
+.ci/workdir/aurora-cli/lib/aurora.js install \
+    --chain 1313161556 \
+    --owner aurora.test.near \
+    --signer aurora.test.near \
+    --engine aurora.test.near \
+    --endpoint https://${NEARCORE_CONTAINER_NAME}:3030 \
+    .ci/workdir/contract.wasm
+
+echo "Sleeping for 5 seconds..."
+sleep 5
+
+
+echo "Creating relayer configuration..."
+cp ~/.near-credentials/local/aurora.test.near.json config/aurora.test.near.json
+cat >config/local.yaml <<EOF
+---
+port: 8545
+database: postgres://aurora:aurora@${DATABASE_CONTAINER_NAME}/aurora
+network: local
+endpoint: http://${NEARCORE_CONTAINER_NAME}:3030
+engine: aurora.test.near
+signer: aurora.test.near
+signerKey: config/aurora.test.near.json
+EOF
+
+echo "Starting database..."
+docker run -d --rm \
+    --network $NETWORK_NAME \
+    --name $DATABASE_CONTAINER_NAME \
+    $DATABASE_IMAGE_NAME
+
+echo "Starting indexer..."
+docker run -d --rm --init \
+    --network $NETWORK_NAME \
+    -e NEAR_ENV=localnet \
+    -e NODE_ENV=localnet \
+    -v ./config:/srv/aurora/relayer/config \
+    --name $INDEXER_CONTAINER_NAME \
+    $ENDPOINT_IMAGE_NAME \
+    node lib/indexer.js
+
+echo "Starting endpoint..."
+docker run -d --rm --init \
+    --network $NETWORK_NAME \
+    -e NEAR_ENV=localnet \
+    -e NODE_ENV=localnet \
+    -v ./config:/srv/aurora/relayer/config \
+    --name $ENDPOINT_CONTAINER_NAME \
+    $ENDPOINT_IMAGE_NAME \
+    node lib/index.js
+
+
+echo "Putting relayer endpoint hostname to .ci/workdir/endpoint.txt"
+echo $ENDPOINT_CONTAINER_NAME > .ci/workdir/endpoint.txt
+
+
+echo "Sleeping for 30 seconds"
+sleep 30
+
+echo "[FOOBAR] DB logs:"
+docker logs $DATABASE_CONTAINER_NAME
+echo "[FOOBAR] Indexer logs:"
+docker logs $INDEXER_CONTAINER_NAME
+echo "[FOOBAR] Endpoint logs:"
+docker logs $ENDPOINT_CONTAINER_NAME
+
+
+echo "Setup finished!"
