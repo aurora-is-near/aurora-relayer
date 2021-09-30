03bcfe6312ed56662935ecb2131e460ca273bd03
Dmitry Strokov <dmitry.strokov@aurora.dev>
2021-09-30 05:26:54 +0300

    Fix typo

--- .ci/cleanup.sh
+++ .ci/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ev
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source SCRIPT_DIR/common.sh
-cd SCRIPT_DIR/..
+source $SCRIPT_DIR/common.sh
+cd $SCRIPT_DIR/..
 
 
 echo "Cleaning docker objects..."
