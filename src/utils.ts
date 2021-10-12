b245fe965d00667405445a58e9c19f4d8e44c9e1
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-10-12 10:28:36 +0200

    minor linting fix

--- src/utils.ts
+++ src/utils.ts
@@ -5,29 +5,25 @@ export function computeBlockHash(
   accountId: string,
   chainId: number
 ): any {
-    return sha256(
-        generateBlockPreImage(
-            blockHeight, 
-            accountId, 
-            chainId
-        )
-    ).toString('hex');
+  return sha256(
+    generateBlockPreImage(blockHeight, accountId, chainId)
+  ).toString('hex');
 }
 
 function generateBlockPreImage(
-    blockHeight: number,
-    accountId: string,
-    chainId: number
+  blockHeight: number,
+  accountId: string,
+  chainId: number
 ): any {
-    const blockHeightBuf = Buffer.alloc(4);
-    blockHeightBuf.writeInt32BE(blockHeight,0);
-    const chainIdBuf = Buffer.alloc(4);
-    chainIdBuf.writeInt32BE(chainId, 0);
-    return  Buffer.concat([
-      Buffer.alloc(29), 
-      chainIdBuf, 
-      Buffer.from(accountId, 'utf8'), 
-      Buffer.alloc(4), 
-      blockHeightBuf
-    ]);
+  const blockHeightBuf = Buffer.alloc(4);
+  blockHeightBuf.writeInt32BE(blockHeight, 0);
+  const chainIdBuf = Buffer.alloc(4);
+  chainIdBuf.writeInt32BE(chainId, 0);
+  return Buffer.concat([
+    Buffer.alloc(29),
+    chainIdBuf,
+    Buffer.from(accountId, 'utf8'),
+    Buffer.alloc(4),
+    blockHeightBuf,
+  ]);
 }
