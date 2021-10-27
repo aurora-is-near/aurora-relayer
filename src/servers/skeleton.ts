f771c5dcc26108b6196562d08ae6465d9a445828
Arto Bendiken <arto@aurora.dev>
2021-10-27 22:40:37 +0300

    Handle IPv6 canonicalization in Go instead of JS.

--- src/servers/skeleton.ts
+++ src/servers/skeleton.ts
@@ -8,9 +8,6 @@ import { bytesToHex, Engine, intToHex } from '@aurora-is-near/engine';
 import { keccakFromHexString } from 'ethereumjs-util';
 import { Logger } from 'pino';
 
-// import ipAddress from 'ip-address';
-// const { Address6 } = ipAddress;
-
 import { spawn } from 'child_process';
 
 export abstract class SkeletonServer implements web3.Service {
@@ -31,11 +28,6 @@ export abstract class SkeletonServer implements web3.Service {
       process.env.CF_ACCOUNT_ID &&
       process.env.CF_LIST_ID
     ) {
-      // if (ip.includes(':')) {
-      //   ip =
-      //     new Address6(ip).canonicalForm().split(':').slice(0, 4).join(':') +
-      //     '::/64';
-      // }
       const subprocess = spawn(
         '/srv/aurora/relayer/util/ban', // FIXME: don't use absolute path
         [ip, reason || ''],
--- util/go.mod
+++ util/go.mod
@@ -6,7 +6,10 @@ require github.com/cloudflare/cloudflare-go v0.26.0
 
 require (
 	github.com/pkg/errors v0.9.1 // indirect
+	go4.org/intern v0.0.0-20210108033219-3eb7198706b2 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20201222180813-1025295fd063 // indirect
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	inet.af/netaddr v0.0.0-20211022222605-2e4260506ddd // indirect
 )
