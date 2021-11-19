c4182840778d152872576792f75e4ac29e911ce7
Arto Bendiken <arto@aurora.dev>
2021-11-19 14:32:38 +0200

    Parallelize the indexer backend to speed up indexing.

---
+++ lib/indexer_worker.d.ts
@@ -0,0 +1,15 @@
+import { Config } from './config.js';
+import { pg } from './database.js';
+import { AccountID, BlockHeight, Engine, LogEvent, NetworkConfig, Transaction } from '@aurora-is-near/engine';
+export declare class Indexer {
+    readonly config: Config;
+    readonly network: NetworkConfig;
+    readonly engine: Engine;
+    protected readonly contractID: AccountID;
+    protected readonly pgClient: pg.Client;
+    constructor(config: Config, network: NetworkConfig, engine: Engine);
+    start(): Promise<void>;
+    indexBlock(blockID: BlockHeight): Promise<void>;
+    indexTransaction(blockID: BlockHeight, transactionIndex: number, transaction: Transaction): Promise<void>;
+    indexEvent(blockID: BlockHeight, transactionIndex: number, transactionID: number, eventIndex: number, event: LogEvent): Promise<void>;
+}
