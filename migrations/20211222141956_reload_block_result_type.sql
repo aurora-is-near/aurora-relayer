f4f51aaca15068b65274eae4b615b77b32c67191
spilin <lyoshakr@gmail.com>
2021-12-22 14:25:39 +0200

    Add migration to reload `block_result` for support changes in #131

---
+++ migrations/20211222141956_reload_block_result_type.sql
@@ -0,0 +1,50 @@
+-- +goose Up
+-- +goose StatementBegin
+DROP TYPE IF EXISTS block_result CASCADE;
+
+CREATE TYPE block_result AS (
+    "number" blockno,
+    "hash" hash,
+    "parentHash" hash,
+    "nonce" bytea,           -- 8 bytes
+    "sha3Uncles" hash,
+    "logsBloom" bytea,       -- 256 bytes
+    "transactionsRoot" hash,
+    "stateRoot" hash,
+    "receiptsRoot" hash,
+    "miner" address,
+    "difficulty" int2,
+    "totalDifficulty" int2,
+    "extraData" bytea,       -- 0 bytes
+    "size" int4,
+    "gasLimit" int8,
+    "gasUsed" int8,
+    "mixHash" hash,
+    "timestamp" int4
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TYPE IF EXISTS block_result CASCADE;
+
+CREATE TYPE block_result AS (
+    "number" blockno,
+    "hash" hash,
+    "parentHash" hash,
+    "nonce" bytea,           -- 8 bytes
+    "sha3Uncles" hash,
+    "logsBloom" bytea,       -- 256 bytes
+    "transactionsRoot" hash,
+    "stateRoot" hash,
+    "receiptsRoot" hash,
+    "miner" address,
+    "difficulty" int2,
+    "totalDifficulty" int2,
+    "extraData" bytea,       -- 0 bytes
+    "size" int4,
+    "gasLimit" int8,
+    "gasUsed" int8,
+    "timestamp" int4
+);
+-- +goose StatementEnd
