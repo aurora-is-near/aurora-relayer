93d156fc006b2366ef4296a77a834c3d4508a84c
zakerikk <97627521+zakerikk@users.noreply.github.com>
2022-04-22 11:29:45 +0100

    Fix eth_newBlockFilter (#270)

--- test/server.test.ts
+++ test/server.test.ts
@@ -36,6 +36,22 @@ describe('AppServer', () => {
     expect(response.body.result).toEqual(`0x0`)
   })
 
+  describe('eth_newBlockFilter', () => {
+    test('should return correct value', async() => {
+      const response = await request(app)
+      .post('/')
+      .send({
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'eth_newBlockFilter',
+        params: [],
+      })
+
+      expect(response.body.result).toContain(`0x`)
+      expect(response.body.result.length).toBe(34)
+    })
+  })
+
   describe('Aurora-Process-Result headers', () => {
     test('array of eth_sendRawTransaction and eth_getCode, result and header should present', async() => {
       const response = await request(app)
