44577462e353fad7552aee1474b35f328fc4063d
Pavel Pustovalov <pustovalov.pavel@gmail.com>
2022-12-05 17:14:08 +0100

    Add migration to update.sh

--- .ci/e2e/controls/relayer/build.sh
+++ .ci/e2e/controls/relayer/build.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/../common.sh
 
 echo "Building database image..."
-time docker build -t $RAW_DATABASE_IMAGE_NAME -f $REPO_ROOT/.docker/Dockerfile.database $REPO_ROOT/.
+time docker build -t $RAW_DATABASE_IMAGE_NAME -f $REPO_ROOT/docker/Dockerfile.database $REPO_ROOT/.
 time docker build -t $DATABASE_IMAGE_NAME --build-arg RAW_DATABASE_IMAGE_NAME=$RAW_DATABASE_IMAGE_NAME -f $REPO_ROOT/.ci/e2e/controls/relayer/Dockerfile.database $REPO_ROOT/.
 
 echo "Building endpoint image..."
-time docker build -t $ENDPOINT_IMAGE_NAME -f $REPO_ROOT/.docker/Dockerfile.endpoint $REPO_ROOT/.
+time docker build -t $ENDPOINT_IMAGE_NAME -f $REPO_ROOT/docker/Dockerfile.endpoint $REPO_ROOT/.
